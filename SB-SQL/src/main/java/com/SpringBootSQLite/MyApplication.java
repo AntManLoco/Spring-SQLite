574d3c5526429b45c74c4e82cd3203923819ed75
UMGCadon <umgcadon@donnie.localdomain>
2023-09-03 15:27:49 -0400

    Initial Proj setup

---
+++ SB-SQL/src/main/java/com/SpringBootSQLite/MyApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootSQLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
---
+++ SB-SQL/src/test/java/com/SpringBootSQLite/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootSQLite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
